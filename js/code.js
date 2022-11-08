555461f20a9bab1dac7b20d9644eb2ab498416e8
Brogy-alt <brogangys2@gmail.com>
2022-11-08 13:42:18 +0200

    only learned 2 today

---
+++ js/code.js
@@ -0,0 +1,51 @@
+//Arithmetic
+
+
+// //addition
+// function addition() {
+//     console.log(2+2);
+// }
+
+
+
+// //subtraction
+// function subtraction() {
+//     console.log(5-2);
+// }
+
+// //multiplication
+// function multiplication() {
+//     console.log(2*2);
+// }
+
+// //division
+// function division() { 
+//     console.log(5/2);
+//  }
+
+//  //exponent
+//  function exponent() {
+//     console.log(2**2);
+//  }
+
+//  //modulus
+// function modulus();
+// console.log(5%20;)
+
+
+
+// //  call functions
+//  addition();
+//  subtraction();
+//  multiplication();
+//  division();
+//  exponent();
+
+
+//Assignments operator
+
+// let numb1 = 4;
+// let numb2 =6;
+// let result = 0;
+// result += numb1 + numb2;
+// console.log(result);
